208f2d2f1c8d02ce5f1c6e1cbac001cbb99530e2
Nick Spence <spence.nick@gmail.com>
2021-01-05 16:38:43 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+# this is code I wrote
+
+
+import re
+
+
+def my_function(one, two, three,
+                four, five, six):
+    print("Hello World!")
+
+
+def second_function():
+    print("Second function")
+
+
+my_list = [1, 2, 3,
+           4, 5, 6]
+
+
+my_list[2]
+
+
+x = 2*52 + 7*2
+
+
+check = True
+if check is True:
+    print("This is true")
+
+
+func_one()
+func_two()
+func_three()
